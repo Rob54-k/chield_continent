e8bdac1c99b80d0207125d2f558b4c4885f8b9ce
Кирилл Захаров <kirillzaharov@MacBook-Air-Kirill.local>
2020-04-06 22:09:28 +0700

    ádd project

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slick_first_contant').slick({
+    //   setting-name: setting-value
+    infinite: true,
+    dots: true,
+    accessibility: false,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+    });
+  });
